390e432363c5e3adbc29f47dbe4ebcc970d6cff4
ravikant <ravikantc17@gmail.com>
2024-07-14 16:11:19 +0530

    netlify api issue

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,16 +1,13 @@
 import axios from "axios";
 
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
-const siteUrl = "https://modelspace.netlify.app/";
-
 export const fetchModelSpacesApi = async () => {
-  return await axios.get(`${API_BASE_URL}/model-spaces`);
+  return await axios.get(`/api/model-spaces`);
 };
 
 export const fetchModelSpaceApi = async (id) => {
-  return await axios.get(`${API_BASE_URL}/model-spaces/${id}`);
+  return await axios.get(`/api/model-spaces/${id}`);
 };
 
 export const handlePredictApi = async (id, inputs) => {
-  return await axios.post(`${siteUrl}/api/model-spaces/${id}/predict`, inputs);
+  return await axios.post(`/api/model-spaces/${id}/predict`, inputs);
 };
