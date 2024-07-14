0fe31d30906507b16719c9e786bdc307f9d755db
ravikant <ravikantc17@gmail.com>
2024-07-14 15:23:49 +0530

    mixed content fix

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+const siteUrl = "https://modelspace.netlify.app/";
 
 export const fetchModelSpacesApi = async () => {
   return await axios.get(`${API_BASE_URL}/model-spaces`);
@@ -11,5 +12,5 @@ export const fetchModelSpaceApi = async (id) => {
 };
 
 export const handlePredictApi = async (id, inputs) => {
-  return await axios.post(`${API_BASE_URL}/model-spaces/${id}/predict`, inputs);
+  return await axios.post(`${siteUrl}/api/model-spaces/${id}/predict`, inputs);
 };
